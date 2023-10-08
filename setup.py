4265be7e5bd6bedc24b6310dca94e361f285a8ef
Caroline <138781771+buatkyc01@users.noreply.github.com>
2023-10-08 10:22:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -76,3 +76,4 @@ setup(
     "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
  "Programming Language :: Python :: 3.8",
+  "Programming Language :: Python :: 3.9",
