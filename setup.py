05fa327c210c5fce36cd3d1f8fd91456703e3ec5
Caroline <138781771+buatkyc01@users.noreply.github.com>
2023-10-16 15:09:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,3 +9,8 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup
+
+# Do not edit these constants. They will be updated automatically
