e976f1a4df34f6b334d344454bc629d2b937ea14
Caroline <138781771+buatkyc01@users.noreply.github.com>
2023-10-18 10:06:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,3 +16,5 @@ from setuptools import setup
 # Do not edit these constants. They will be updated automatically
 # by scripts/update-client.sh.
 CLIENT_VERSION = "27.0.0+snapshot"
+PACKAGE_NAME = "kubernetes"
+DEVELOPMENT_STATUS = "3 - Alpha"
